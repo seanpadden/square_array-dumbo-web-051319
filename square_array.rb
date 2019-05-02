43420131de9bdccac32ea4d78f52181cdf031059
Sean Padden <sean.padden7@gmail.com>
2019-05-02 21:09:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   squared_array = []
-  sarray.each do |num|
-    num ** 2
-  endquared_array = 
+  squared_array = array.each do |num|
+    squared_array << num ** 2
+  end
   squared_array
 end
 
